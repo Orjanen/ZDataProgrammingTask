dbadf9d6b974978d5882c647997aed70ef5d401e
orjanen <superszura@gmail.com>
2021-04-29 12:45:20 +0200

    init

---
+++ ZDataProgrammingTask/Controllers/LoanController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZDataProgrammingTask.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController
+    {
+        
+    }
+}
