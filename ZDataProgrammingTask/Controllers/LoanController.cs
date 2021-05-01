c8d91ca946e51640a9dddf88153de153db263f79
orjanen <superszura@gmail.com>
2021-05-01 10:47:15 +0200

    added a baseController and removed the WetherForcastController

--- ZDataProgrammingTask/Controllers/LoanController.cs
+++ ZDataProgrammingTask/Controllers/LoanController.cs
@@ -1,11 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZDataProgrammingTask.Data;
+using ZDataProgrammingTask.Models;
 
 namespace ZDataProgrammingTask.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class LoanController
+  
+    public class LoanController : BaseApiController
     {
-        
+        private readonly DataContext _context;
+
+        public LoanController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LoanType>>> GetLoanTypes()
+        {
+            return await _context.LoanTypes.ToListAsync();
+        }
     }
 }
